01fc1212b2fcc93561fae4cc04cad68267d9bc65
Gutyiii <gutyeszbandesz@gmail.com>
2021-03-17 09:56:11 +0100

    alapok

---
+++ public_html/script.js
@@ -0,0 +1,44 @@
+//var nev = document.getElementById("adat").value;
+
+function feltolt(tomb) {
+    var darab = Number(document.getElementById("adat").value);
+//    tomb[0]=1;
+//    tomb=new Array();
+    for (var i = 0; i < darab; i++) {
+        var vel = Math.floor(Math.random() * 10) + 1;
+        tomb[i] = vel;
+    }
+}
+
+
+function dolgozz() {
+    var tomb = [];
+//    document.write("Jó reggelt " + nev);                                uj oldalra irja ki
+//    alert("Jó reggelt " + nev);                                         fenti kis ablkaba írja ki
+//    document.getElementById("szoveg").innerHTML = "Jó reggelt " + nev;  //p tagbe irja ki
+//    console.log("Hello");                                               consolra kiiras
+
+    feltolt(tomb);
+    console.log(tomb);
+    document.getElementById("eredmeny").innerHTML = "Az eredmény: <br> " + tomb.join("; ");
+
+    osszegzes(tomb);
+}
+
+function osszegzes(tomb) {
+    var osszeg = 0;
+    for (var i = 0; i < tomb.length; i++) {
+        osszeg += tomb[i];
+    }
+    document.getElementById("osszeg").innerHTML = "Az összeg: " + osszeg;
+}
+
+window.addEventListener("load", init);
+
+function init() {
+    console.log("Első üzenet");
+    document.getElementById("eredmeny").innerHTML = "Rajta vagyok";
+    document.getElementById("oke").addEventListener("click", dolgozz);
+}
+
+
